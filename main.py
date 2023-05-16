5e228f06bf3aa29ae6cf01b4ddef0bdc1b3954a0
Queli Gramacho <112115486+Quelii@users.noreply.github.com>
2023-05-16 16:05:36 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    chato = "você mucilon"
+    eu = "felicidade"
+
+    for i in range(1, 11):
+        print(f"Quando estou perto de {chato},meu nivel de Alegria só vai subindo {i}.")
+    
+    print(f"Suas maluqices são a minha fonte de {eu} infinita.")
+
+if __name__ == "__main__":
+    main()
